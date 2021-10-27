4a5096a3a2e732ed3d97340f9de51301f427ec3d
Colm Flanagan <z5122189@unsw.edu.au>
2021-10-26 21:17:15 -0700

    adding initial working code

---
+++ install/comp3431_starter/share/comp3431_starter/cmake/comp3431_starterConfig.cmake
@@ -0,0 +1,1 @@
+/home/rsa2021/starter_code/build/comp3431_starter/ament_cmake_core/comp3431_starterConfig.cmake
---
+++ install/comp3431_starter/share/comp3431_starter/local_setup.zsh
@@ -0,0 +1,1 @@
+/home/rsa2021/starter_code/build/comp3431_starter/ament_cmake_environment_hooks/local_setup.zsh
